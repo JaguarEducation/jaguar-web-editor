5975ebaa4429a31a383e9b14c69f741ed9b3fbc2
2021 DSM31 Angel Flores Carlos <angelfc.contact@gmail.com>
2021-08-30 00:42:54 -0500

    Primer vistazo

--- js/index.js
+++ js/index.js
@@ -1,61 +1,91 @@
 let html = '', js = '', css = ''
 
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' } });
-window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+window.editor = monaco.editor.create(document.getElementById('code'), {
+    value: 'Empieza creando un nuevo archivo',
+    language: '',
+    theme: 'vs-dark',
+    readOnly: true,
+    automaticLayout: true
+  });
 
-let proxy = URL.createObjectURL(new Blob([`
-	self.MonacoEnvironment = {
-		baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
-	};
-	importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
-`], { type: 'text/javascript' }));
-
-require(["vs/editor/editor.main"], function () {
-    let editorHtml = monaco.editor.create(document.getElementById('html'), {
-        value: [
-            '<button onclick="hi()">',
-            '\tClick me',
-            '</button>'
-        ].join('\n'),
-        language: 'html',
-        theme: 'vs-dark'
-    });
-    let editorCss = monaco.editor.create(document.getElementById('css'), {
-        value: [
-            'body {',
-            '\tmargin: 0;',
-            '\tpadding: 0;',
-            '}'
-        ].join('\n'),
-        language: 'css',
-        theme: 'vs-dark'
-    });
-    let editorJs = monaco.editor.create(document.getElementById('js'), {
-        value: [
-            'function hi() {',
-            '\tconsole.log("Hello world!");',
-            '}'
-        ].join('\n'),
-        language: 'javascript',
-        theme: 'vs-dark'
-    });
     
+
+   console.log(editor.getModel())
+   
+   var model = editor.getModel();
+   // we'll create a model for you if the editor created from string value.
+   //monaco.editor.setModelLanguage(model, "javascript")
+   
+   console.log(editor.getModel())
+
+    emmetMonaco.emmetHTML(monaco);
+    var winPrint = window.open('about:blank', '_blank');
+    
+    function save(){
+        document.getElementById('output').src = "data:text/html;charset=utf-8," + escape(editor.getValue());
+		winPrint.location.reload()
+        //winPrint.document.write(editor.getValue());	
+        winPrint.document.write(editor.getValue());	
+    }
+        //document.getElementById('code').getElementsByTagName('textarea')[0].oninput = save
+
+    editor.layout({});
+
+    //return {a:'ajaja'}
+    /*
     save()
 
     function save() {
-        html = editorHtml.getValue()
+        html = editor.getValue()
         css = editorCss.getValue()
         js = editorJs.getValue()
         // get the value of the data
-        document.getElementById('output').src = "data:text/html;charset=utf-8," + escape(`<html><style>${css}</style><body>${html}</body><script>${js}</script></html>`);
     }
-    document.getElementById('html').getElementsByTagName('textarea')[0].onblur = save
     document.getElementById('js').getElementsByTagName('textarea')[0].onblur = save
     document.getElementById('css').getElementsByTagName('textarea')[0].onblur = save
+    */
     
+//});
+
+//console.log(mona.a)
+
+//editor.setValue('xdd')
+
+var handler = document.querySelector('.handler');
+var wrapper = handler.closest('.container');
+var boxA = wrapper.querySelector('.box');
+var isHandlerDragging = false;
+
+document.addEventListener('mousedown', function(e) {
+  // If mousedown event is fired from .handler, toggle flag to true
+  if (e.target === handler) {
+    isHandlerDragging = true;
+  }
 });
 
-function cssPanel() {
-    if(document.getElementById('css'))
-    document.getElementById('css').classList.add('d-none')
-}
+document.addEventListener('mousemove', function(e) {
+  // Don't do anything if dragging flag is false
+  if (!isHandlerDragging) {
+    return false;
+  }
+
+  // Get offset
+  var containerOffsetLeft = wrapper.offsetLeft;
+
+  // Get x-coordinate of pointer relative to container
+  var pointerRelativeXpos = e.clientX - containerOffsetLeft;
+  
+  // Arbitrary minimum width set on box A, otherwise its inner content will collapse to width of 0
+  var boxAminWidth = 60;
+
+  // Resize box A
+  // * 8px is the left/right spacing between .handler and its inner pseudo-element
+  // * Set flex-grow to 0 to prevent it from growing
+  boxA.style.width = (Math.max(boxAminWidth, pointerRelativeXpos - 8)) + 'px';
+  boxA.style.flexGrow = -100;
+});
+
+document.addEventListener('mouseup', function(e) {
+  // Turn off dragging flag when user mouse is up
+  isHandlerDragging = false;
+});
---
+++ js/virtualFiles.js
@@ -0,0 +1,74 @@
+let tree = document.getElementById('tree');
+let documentOpened = ''
+
+
+function newFile() {
+    let name = prompt('Escribe el nombre del archivo');
+    event.preventDefault()
+    console.log(name);
+    if (name == null) {
+        return
+    }
+    else {
+        createFile(name)
+    }
+}
+
+function createFile(name) {
+    let p = document.createElement('p')
+    p.textContent = name
+    name = name.split('.').join("");
+    eval('window.' + name + '= ""');
+    p.onclick = function () {
+        console.log(p.textContent.split('.').join(""))
+        console.log(eval(p.textContent.split('.').join("")))
+        //console.log(p.textContent.split('.').pop())
+        //console.log(name)
+        setLang(p.textContent.split('.').pop())
+        editor.setValue(eval(p.textContent.split('.').join("")));
+        editor.updateOptions({ readOnly: false, });
+        documentOpened = p.textContent.split('.').join("")
+    }
+    tree.appendChild(p)
+}
+
+function setContent() {
+    editor.setValue('aaa')
+}
+
+function setLang(ext) {
+    switch (ext) {
+        case 'html':
+            var model = editor.getModel();
+            monaco.editor.setModelLanguage(model, "html")
+            break;
+        case 'css':
+            var model = editor.getModel();
+            monaco.editor.setModelLanguage(model, "css")
+            break;
+        case 'js':
+            var model = editor.getModel();
+            monaco.editor.setModelLanguage(model, "javascript")
+            break;
+
+        default:
+            var model = editor.getModel();
+            monaco.editor.setModelLanguage(model, "")
+            break;
+    }
+}
+
+document.addEventListener('keydown', e => {
+    if (e.ctrlKey && e.key === 's') {
+        // Prevent the Save dialog to open
+        e.preventDefault();
+        // Place your code here
+        console.log('CTRL + S');
+        console.log(documentOpened)
+        var editorValue = editor.getValue()
+        console.log(editor.getValue())
+        eval(documentOpened + " = " + '`' + editorValue + '`')
+        console.log(eval(documentOpened))
+        save()
+    }
+});
